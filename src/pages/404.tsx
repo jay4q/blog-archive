c170ee6cb2a790c4a4911098cd8e2aed5df33baf
jay4q <jay4qqqq@gmail.com>
2021-04-14 17:59:34 +0800

    upd: 优化404页

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import { Fragment, FunctionComponent } from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
 import Lottie from 'react-lottie-player'
 
 const lottieData = require('@/assets/404.json')
@@ -12,19 +11,17 @@ const Index: FunctionComponent = () => {
   return (
     <Fragment>
       <Head>
-        <title>🙇 oops ~</title>
+        <title>🙇 404 🙇</title>
       </Head>
-      <div className='flex-grow flex flex-col items-center pt-8'>
-        <Lottie
-          loop
-          play
-          animationData={lottieData}
-          style={{ width: 256, height: 256 }}
-        />
-        <h1 className='text-xl mb-4 font-bold -mt-4 dark:text-white'>OOPS NOTHING HERE :P</h1>
-        <Link href='/'>
-          <a className='text-sm text-gray-500'>🏠&ensp;back home</a>
-        </Link>
+      <div className='w-full h-full flex flex-col items-center justify-center'>
+        <div className='w-80 h-80 lg:w-96 lg:h-96'>
+          <Lottie
+            loop
+            play
+            animationData={lottieData}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </div>
       </div>
     </Fragment>
   )
